37a58b37e6d856fb16985673ee890b8ecea7e549
Daniel Pecos Martinez <contact@danielpecos.com>
2012-05-28 13:45:22 +0200

    Added test infrastructure

---
+++ app-test.js
@@ -0,0 +1,22 @@
+Ext.Loader.setConfig({enabled:true});
+
+Ext.require('Ext.app.Application');
+
+var Application = null;
+
+Ext.onReady(function() {
+
+	Application = Ext.create('Ext.app.Application', {
+
+		name: 'KR',
+		controllers: ['Entry'],
+		
+		launch: function() {
+			jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
+			jasmine.getEnv().execute();
+		}
+		
+	});
+
+});
+
