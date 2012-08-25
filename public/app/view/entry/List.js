cd975913604854854a231bb16d511fe11c88125d
Daniel Pecos Martinez <contact@danielpecos.com>
2012-08-25 21:44:16 +0200

    Closes #47: Make URL and email columns linkable

--- public/app/view/entry/List.js
+++ public/app/view/entry/List.js
@@ -13,10 +13,10 @@ Ext.define('KR.view.entry.List', {
       this.columns = [
          {header: 'Category', dataIndex: 'category', flex: 1},
          {header: 'Name', dataIndex: 'name', flex: 1},
-         {header: 'URL', dataIndex: 'url', flex: 1},
+         {header: 'URL', dataIndex: 'url', flex: 1, xtype: 'templatecolumn', tpl: '<tpl if="url"><a href="{url}">{url}</a></tpl>'},
          {header: 'User', dataIndex: 'cleartext_user', flex: 1, hidden: true},
          {header: 'Password', dataIndex: 'cleartext_password', flex: 1, hidden: true},
-         {header: 'Email', dataIndex: 'email', flex: 1},
+         {header: 'Email', dataIndex: 'email', flex: 1, xtype: 'templatecolumn', tpl: '<tpl if="email"><a href="mailto:{email}">{email}</a></tpl>'},
          {header: 'Notes', dataIndex: 'notes', flex: 1}
       ];
 
