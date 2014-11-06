c208e974b07aabcaf35258817f3add18b4e3320c
Daniel Pecos Martinez <contact@danielpecos.com>
2014-11-06 08:36:04 +0100

    Entries can be inserted (WIP)

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -5,21 +5,38 @@
 angular.module('myApp.controllers', [])
 
   .controller('MainCtrl', ['$rootScope', '$q', '$modal', 'categoriesDAO', function($rootScope, $q, $modal, categoriesDAO) {
-     //$q.when(categoriesDAO.load()).then(function(categories) { $rootScope.categories = categories; });
-     $rootScope.categories = [{name:'foo'}];
+     $rootScope.categories = [{name: 'foo'}];
+     $rootScope.entries = [];
 
      this.showCategories = function() {
         var modalInstance = $modal.open({
            templateUrl: 'partials/categories.html',
-           controller: 'CategoriesCtrl as Ctrl',
-           resolve: {
-              /*categories: function() {
-                 return $scope.categories;
-              }*/
-           }
+           controller: 'CategoriesCtrl as Ctrl'
         });
 
         modalInstance.result.then(null, function(categories) {
+           // TODO
+           console.log(arguments);
+        });
+     };
+
+     this.addNewEntry = function() {
+        var modalInstance = $modal.open({
+           templateUrl: 'partials/addEntry.html',
+           controller: 'EditEntryCtrl as Ctrl',
+           resolve: {
+              entry: function() {
+                 return { };
+              }
+           }
+        });
+
+        modalInstance.result.then(function(newEntry) {
+           // TODO
+           console.log(arguments);
+        }, function() {
+           // dialog closed without saving
+           // TODO
            console.log(arguments);
         });
      };
@@ -36,15 +53,30 @@ angular.module('myApp.controllers', [])
      };
   }])
 
+  .controller('EditEntryCtrl', ['$rootScope', '$scope', '$q', '$modalInstance', 'entriesDAO', 'entry', function($rootScope, $scope, $q, $modalInstance, entriesDAO, entry) {
+     $scope.entry = entry;
+
+     this.save = function() {
+        $q.when(entriesDAO.create($scope.entry)).then(function(entry) { 
+           $rootScope.entries.push(entry);
+           $modalInstance.close($scope.entry);
+        }, function(err) {
+           console.log(err);
+           alert("Error creating entry: " + err.statusText);
+        });
+     };
+  }])
+
   .controller('EditCategoryCtrl', ['$scope', '$modalInstance', 'category', function($scope, $modalInstance, category) {
      $scope.category = category;
 
      this.save = function() {
-        $modalInstance.close($scope.category);
+        $modalInstance.close($scope.entry);
      };
   }])
 
   .controller('CategoriesCtrl', ['$rootScope', '$scope', '$q', '$modal', 'categoriesDAO', function($rootScope, $scope, $q, $modal, categoriesDAO) {
+     //$q.when(categoriesDAO.load()).then(function(categories) { $rootScope.categories = categories; });
 
      this.remove = function(category) {
         $rootScope.categories = $rootScope.categories.filter(function(cat) {
--- public/app/js/factories.js
+++ public/app/js/factories.js
@@ -9,7 +9,7 @@ angular.module('myApp.factories', [])
         }
      };
   })
-  .factory("entriesDAO", function() {
+  .factory("entriesDAO", function($http) {
      return {
         load: function() {
            return [
@@ -17,6 +17,9 @@ angular.module('myApp.factories', [])
            {name: "Entry 2", url: "http://", user: "User 1", email: "user1@email.com", password: "password1", notes: "Notes for entry 1", category: "bar"},
            {name: "Entry 2", url: "http://", user: "User 1", email: "user1@email.com", password: "password1", notes: "Notes for entry 1", category: "foo"}
            ]
+        },
+        create: function(entry) {
+           return $http.post("/data/entry", entry);
         }
      };
   });
