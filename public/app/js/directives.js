5406e0b8e1e3b413118447ad23934ad8c18337b5
Daniel Pecos Martinez <contact@danielpecos.com>
2014-11-26 18:56:09 +0100

    Sensitive data revealing moved to a directive

--- public/app/js/directives.js
+++ public/app/js/directives.js
@@ -8,4 +8,14 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  .directive('reveal', function() {
+     return {
+        restrict: 'A',
+        replace: false,
+        scope: {
+           data: '=data'
+        },
+        templateUrl: 'partials/directives/reveal.html'
+     };
+  });
