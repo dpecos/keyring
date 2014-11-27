facb22a3bb37d07c3e7e4dc58e274a8ef6c1e702
Daniel Pecos Martinez <contact@danielpecos.com>
2014-11-27 18:51:21 +0100

    Editing entries working

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -2,175 +2,196 @@
 
 /* Controllers */
 
+function showEntryDialog(entry, $modal) {
+  entry = entry || {};
+
+  var previousCipheredPassword = entry.password;
+  entry.password = entry.clearPassword;
+
+  var modalInstance = $modal.open({
+    templateUrl: 'partials/addEntry.html',
+    controller: 'EditEntryCtrl as Ctrl',
+    resolve: {
+      entry: function() {
+        return entry;
+      }
+    }
+  });
+
+  modalInstance.result.then(function(newEntry) {
+    // TODO
+    console.log(arguments);
+  }, function() {
+    //cancel
+    entry.password = previousCipheredPassword;
+  });
+}
+
 angular.module('myApp.controllers', [])
 
   .controller('MainCtrl', ['$rootScope', '$q', '$modal', 'categoriesDAO', 'entriesDAO', 'cryptoSRV', function($rootScope, $q, $modal, categoriesDAO, entriesDAO, cryptoSRV) {
-     $rootScope.categories = [];
-     $rootScope.entries = [];
+    $rootScope.categories = [];
+    $rootScope.entries = [];
 
-     this.showCategories = function() {
-        var modalInstance = $modal.open({
-           templateUrl: 'partials/categories.html',
-           controller: 'CategoriesCtrl as Ctrl'
-        });
+    this.showCategories = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'partials/categories.html',
+        controller: 'CategoriesCtrl as Ctrl'
+      });
 
-        modalInstance.result.then(null, function(categories) {
-           // TODO
-           console.log(arguments);
-        });
-     };
+      modalInstance.result.then(null, function(categories) {
+        // TODO
+        console.log(arguments);
+      });
+    };
 
-     this.addNewEntry = function() {
-        var modalInstance = $modal.open({
-           templateUrl: 'partials/addEntry.html',
-           controller: 'EditEntryCtrl as Ctrl',
-           resolve: {
-              entry: function() {
-                 return { };
-              }
-           }
-        });
+    this.addNewEntry = function() {
+      showEntryDialog({}, $modal);
+    };
 
-        modalInstance.result.then(function(newEntry) {
-           // TODO
-           console.log(arguments);
-        }, function() {
-           // dialog closed without saving
-           // TODO
-           console.log(arguments);
-        });
-     };
+    var loadCategories = function() {
+      return $q.when(categoriesDAO.load()).then(function(payload) { $rootScope.categories = payload.data.data; });
+    };
 
-     var loadCategories = function() {
-        return $q.when(categoriesDAO.load()).then(function(payload) { $rootScope.categories = payload.data.data; });
-     };
-     
-     var loadEntries = function() {
-        return $q.when(entriesDAO.load()).then(function(entries) { 
-           $rootScope.entries = entries.data.data; 
-           $rootScope.checkLock();
-        });
-     };
+    var loadEntries = function() {
+      return $q.when(entriesDAO.load()).then(function(entries) { 
+        $rootScope.entries = entries.data.data; 
+        $rootScope.checkLock();
+      });
+    };
 
-     $rootScope.checkLock = function() {
-        $rootScope.entries = $rootScope.entries.map(function(entry) {
-           if ($rootScope.masterPassword) {
-              entry.clearPassword = cryptoSRV.decrypt(entry.password, $rootScope.masterPassword);
-           } else {
-              delete entry.clearPassword;
-           }
-           return entry;
-        });
-     };
+    $rootScope.checkLock = function() {
+      $rootScope.entries = $rootScope.entries.map(function(entry) {
+        if ($rootScope.masterPassword) {
+          entry.clearPassword = cryptoSRV.decrypt(entry.password, $rootScope.masterPassword);
+        } else {
+          delete entry.clearPassword;
+        }
+        return entry;
+      });
+    };
 
-     $rootScope.reloadData = function() {
-        loadCategories().then(loadEntries);
-     };
+    $rootScope.reloadData = function() {
+      loadCategories().then(loadEntries);
+    };
 
-     $rootScope.reloadData();
+    $rootScope.reloadData();
 
   }])
 
   .controller('MasterPasswordCtrl', ['$rootScope', '$scope', '$modal', function($rootScope, $scope, $modal) {
-     this.show = function() {
-        var modalInstance = $modal.open({
-           templateUrl: 'partials/masterPassword.html',
-           controller: 'ChangeMasterPasswordCtrl as Ctrl',
-           resolve: {
-              masterPassword: function() {
-                 return "";
-              }
-           }
-        });
+    this.show = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'partials/masterPassword.html',
+        controller: 'ChangeMasterPasswordCtrl as Ctrl',
+        resolve: {
+          masterPassword: function() {
+            return "";
+          }
+        }
+      });
 
-        modalInstance.result.then(function(password) {
-           $rootScope.masterPassword = password;
-           $rootScope.checkLock();
-        }, function() {
-           $rootScope.masterPassword = null;
-           $rootScope.checkLock();
-        });
-     };
+      modalInstance.result.then(function(password) {
+        $rootScope.masterPassword = password;
+        $rootScope.checkLock();
+      }, function() {
+        $rootScope.masterPassword = null;
+        $rootScope.checkLock();
+      });
+    };
 
   }])
 
   .controller('ChangeMasterPasswordCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
-     this.accept = function(keyEvent) {
-        if (typeof(keyEvent) == "undefined" || keyEvent.which == 13) {
-           $modalInstance.close($scope.masterPassword);
-        }
-     };
+    this.accept = function(keyEvent) {
+      if (typeof(keyEvent) == "undefined" || keyEvent.which == 13) {
+        $modalInstance.close($scope.masterPassword);
+      }
+    };
 
-     this.cancel = function() {
-        $modalInstance.close();
-     };
+    this.cancel = function() {
+      $modalInstance.close();
+    };
 
   }])
 
-  .controller('EntriesCtrl', ['$rootScope', '$scope', '$q', 'entriesDAO', function($rootScope, $scope, $q, entriesDAO) {
-     this.entriesByCategory = function(category) {
-        return $rootScope.entries.filter(function(entry) {
-           return entry.category === category.name;
-        });
-     };
+  .controller('EntriesCtrl', ['$rootScope', '$scope', '$q', '$modal', 'entriesDAO', function($rootScope, $scope, $q, $modal, entriesDAO) {
+    this.entriesByCategory = function(category) {
+      return $rootScope.entries.filter(function(entry) {
+        return entry.category === category.name;
+      });
+    };
+
+    this.editEntry = function(entry) {
+      showEntryDialog(entry, $modal);
+    };
   }])
 
   .controller('EditEntryCtrl', ['$rootScope', '$scope', '$q', '$modalInstance', 'entriesDAO', 'cryptoSRV', 'entry', function($rootScope, $scope, $q, $modalInstance, entriesDAO, cryptoSRV, entry) {
-     $scope.entry = entry;
+    $scope.entry = entry;
 
-     this.save = function() {
-        entry.clearPassword = entry.password;
-        entry.password = cryptoSRV.encrypt(entry.clearPassword,  $rootScope.masterPassword);
+    this.save = function() {
+      entry.clearPassword = entry.password;
+      entry.password = cryptoSRV.encrypt(entry.clearPassword,  $rootScope.masterPassword);
 
-        $q.when(entriesDAO.create($scope.entry)).then(function(entry) { 
-           $rootScope.entries.push($scope.entry);
-           $modalInstance.close($scope.entry);
+      if (entry._id) {
+        $q.when(entriesDAO.update(entry)).then(function(entry) { 
+          $modalInstance.close(entry);
         }, function(err) {
-           console.log(err);
-           alert("Error creating entry: " + err.statusText);
+          console.log(err);
+          alert("Error updating entry: " + err.statusText);
         });
-     };
+      } else {
+        $q.when(entriesDAO.create(entry)).then(function(entry) { 
+          $rootScope.entries.push(entry);
+          $modalInstance.close(entry);
+        }, function(err) {
+          console.log(err);
+          alert("Error creating entry: " + err.statusText);
+        });
+      }
+    };
   }])
 
   .controller('CategoriesCtrl', ['$rootScope', '$scope', '$q', '$modal', 'categoriesDAO', function($rootScope, $scope, $q, $modal, categoriesDAO) {
-     this.remove = function(category) {
-        $q.when(categoriesDAO.remove(category)).then(function(error) {
-           //TODO: handle error
-           $rootScope.categories = $rootScope.categories.filter(function(cat) {
-              return cat.name != category.name;
-           });
+    this.remove = function(category) {
+      $q.when(categoriesDAO.remove(category)).then(function(error) {
+        //TODO: handle error
+        $rootScope.categories = $rootScope.categories.filter(function(cat) {
+          return cat.name != category.name;
         });
-     };
+      });
+    };
 
-     this.add = function() {
-        var modalInstance = $modal.open({
-           templateUrl: 'partials/editCategory.html',
-           controller: 'EditCategoryCtrl as Ctrl',
-           //scope: $scope,
-           resolve: {
-              category: function() {
-                 return { name: null };
-              }
-           }
-        });
+    this.add = function() {
+      var modalInstance = $modal.open({
+        templateUrl: 'partials/editCategory.html',
+        controller: 'EditCategoryCtrl as Ctrl',
+        //scope: $scope,
+        resolve: {
+          category: function() {
+            return { name: null };
+          }
+        }
+      });
 
-        modalInstance.result.then(function(newCategory) {
-           $q.when(categoriesDAO.create(newCategory)).then(function(category) { 
-              $rootScope.reloadData();
-           }, function(err) {
-              console.log(err);
-              alert("Error creating category: " + err.statusText);
-           });
-        }, function() {
-           // dialog closed without saving
+      modalInstance.result.then(function(newCategory) {
+        $q.when(categoriesDAO.create(newCategory)).then(function(category) { 
+          $rootScope.reloadData();
+        }, function(err) {
+          console.log(err);
+          alert("Error creating category: " + err.statusText);
         });
-     };
+      }, function() {
+        // dialog closed without saving
+      });
+    };
   }])
 
   .controller('EditCategoryCtrl', ['$scope', '$modalInstance', 'category', function($scope, $modalInstance, category) {
-     $scope.category = category;
+    $scope.category = category;
 
-     this.save = function() {
-        $modalInstance.close($scope.category);
-     };
+    this.save = function() {
+      $modalInstance.close($scope.category);
+    };
   }]);
--- public/app/js/factories.js
+++ public/app/js/factories.js
@@ -1,24 +1,27 @@
 angular.module('myApp.factories', [])
   .factory("categoriesDAO", function($http) {
-     return {
-        load: function() {
-           return $http.get("/data/category");
-        }, 
-        create: function(category) {
-           return $http.post("/data/category", category);
-        },
-        remove: function(category) {
-           return $http.delete("/data/category/" + category._id);
-        }
-     };
+    return {
+      load: function() {
+        return $http.get("/data/category");
+      }, 
+      create: function(category) {
+        return $http.post("/data/category", category);
+      },
+      remove: function(category) {
+        return $http.delete("/data/category/" + category._id);
+      }
+    };
   })
   .factory("entriesDAO", function($http) {
-     return {
-        load: function() {
-           return $http.get("/data/entry");
-        },
-        create: function(entry) {
-           return $http.post("/data/entry", entry);
-        }
-     };
+    return {
+      load: function() {
+        return $http.get("/data/entry");
+      },
+      create: function(entry) {
+        return $http.post("/data/entry", entry);
+      },
+      update: function(entry) {
+        return $http.put("/data/entry/" + entry._id, entry);
+      }
+    };
   });
