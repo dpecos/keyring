73800f9e5fecafe4aa202c53afce2e464fb7b99e
Daniel Pecos Martinez <contact@danielpecos.com>
2015-01-13 08:25:04 +0100

    AJAX urls relative to context path

--- public/app/js/factories.js
+++ public/app/js/factories.js
@@ -1,30 +1,32 @@
+var contextPath = window.location.pathname.substring(0, window.location.pathname.indexOf('/app'));
+
 angular.module('KeyRing.factories', [])
   .factory("categoriesDAO", function($http) {
     return {
       load: function() {
-        return $http.get("/data/category");
+        return $http.get(contextPath + "/data/category");
       }, 
       create: function(category) {
-        return $http.post("/data/category", category);
+        return $http.post(contextPath + "/data/category", category);
       },
       remove: function(category) {
-        return $http.delete("/data/category/" + category._id);
+        return $http.delete(contextPath + "/data/category/" + category._id);
       }
     };
   })
   .factory("entriesDAO", function($http) {
     return {
       load: function() {
-        return $http.get("/data/entry");
+        return $http.get(contextPath + "/data/entry");
       },
       create: function(entry) {
-        return $http.post("/data/entry", entry);
+        return $http.post(contextPath + "/data/entry", entry);
       },
       update: function(entry) {
-        return $http.put("/data/entry/" + entry._id, entry);
+        return $http.put(contextPath + "/data/entry/" + entry._id, entry);
       },
       remove: function(entry) {
-        return $http.delete("/data/entry/" + entry._id);
+        return $http.delete(contextPath + "/data/entry/" + entry._id);
       }
     };
   });
