008bdc8d008ae6bbafb8ada271ac66fc4d830f17
Daniel Pecos Martinez <contact@danielpecos.com>
2015-06-11 08:24:49 +0200

    Fixes #53: Google OpenId deprecated

--- routes/auth-google.js
+++ routes/auth-google.js
@@ -1,5 +1,5 @@
 var passport = require('passport');
-var GoogleStrategy = require('passport-google').Strategy;
+var GoogleStrategy = require('passport-google-openidconnect').Strategy;
 
 passport.serializeUser(function(user, done) {
    done(null, user);
@@ -15,23 +15,25 @@ module.exports = function(app) {
 
    passport.use(new GoogleStrategy(
       {
-         returnURL: url('/auth/google/callback'),
-         realm: url()
+         clientID: '385034162376-hjbif1ag4u19omoi1g87uj4rb1lj6dcb.apps.googleusercontent.com',
+         clientSecret: 'PaXTS-MCy_mm47qQAXiA_4YH',
+         callbackURL: url('/auth/google/callback'),
+         userInfoURL: "https://www.googleapis.com/plus/v1/people/me"
       },
-      function(identifier, profile, done) {
-         profile.uid = profile.emails[0].value;
-         profile.uname = profile.displayName;
+      function(iss, sub, profile, accessToken, refreshToken, done) {
+         profile.uid = profile._json.emails[0].value;
+         profile.uname = profile._json.displayName;
          app.db.checkUserExists(profile, function() {
             done(null, profile);
          });
       }
    ));
 
-   app.get('/auth/google', passport.authenticate('google', {failureRedirect: url('/login')}), function(req, res) {
+   app.get('/auth/google', passport.authenticate('google-openidconnect', {scope: ['email', 'profile'], failureRedirect: url('/login')}), function(req, res) {
       res.redirect(url('/'));
    });
 
-   app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: url('/login')}), function(req, res) {
+   app.get('/auth/google/callback', passport.authenticate('google-openidconnect', {scope: ['email', 'profile'], failureRedirect: url('/login')}), function(req, res) {
       res.redirect(url('/'));
    }); 
 
